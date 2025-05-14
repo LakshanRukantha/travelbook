ac252b2acef780173bf8549d4fece0b24def780d
shanikameedum <admshanika@students.nsbm.ac.lk>
2025-05-14 16:10:50 +0530

    sign up feature 🚀

---
+++ lib/services/authentication.dart
@@ -0,0 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthServices {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<String> signUpUser({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    String res = "Some error Occured";
+    try {
+      UserCredential credential = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      await credential.user!.updateDisplayName(name);
+      await credential.user!.reload();
+
+      await _firestore.collection("users").doc(credential.user!.uid).set({
+        'name': name,
+        'email': email,
+        'uid': credential.user!.uid,
+      });
+      res = "success";
+    } catch (e) {
+      res = e.toString();
+    }
+    return res;
+  }
+}
